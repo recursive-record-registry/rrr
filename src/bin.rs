7bed147f60186a690bc25f5a91c76832461f971f
Jakub Hlusička <Limeth@users.noreply.github.com>
2024-07-13 21:04:38 +0200

    Setup tracing for the executable binary

--- src/bin.rs
+++ src/bin.rs
@@ -1,11 +1,25 @@
 #![feature(async_closure)]
 
 use clap::Parser;
+use color_eyre::eyre::Result;
 use rrr::cmd::RrrArgs;
+use tracing_error::ErrorLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+async fn setup_tracing() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(ErrorLayer::default())
+        .with(EnvFilter::from_default_env())
+        .try_init()?;
+
+    Ok(())
+}
 
 #[tokio::main]
-async fn main() -> color_eyre::Result<()> {
-    let args = RrrArgs::parse();
+async fn main() -> Result<()> {
+    setup_tracing().await?;
+    RrrArgs::parse().process().await?;
 
-    args.process().await
+    Ok(())
 }
